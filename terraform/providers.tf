454a2cbf6492ab1317986962c18ba681baaa706e
Harshit <harshitjain137@gmail.com>
2026-02-14 13:40:41 +0530

    used terraform import and default tags usecase

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,10 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      Owner          = "Harshit"
+      Provisioned_by = "Terraform"
+    }
+  }
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,21 @@
 terraform {
   required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0.0"
+    }
+  }
 }
